e35d27bcb240289c1a75698c3fd18a0daaae07e8
Pratham22R <pratham2262003@gmail.com>
2025-11-18 20:50:40 +0530

    Implement search stock feature

--- components/Navitems.tsx
+++ components/Navitems.tsx
@@ -1,31 +1,51 @@
-'use client';
-import { usePathname } from "next/navigation";
+"use client";
+
 import { NAV_ITEMS } from "@/lib/constant";
 import Link from "next/link";
-import React from "react";
+import { usePathname } from "next/navigation";
+import SearchCommand from "@/components/SearchCommand";
 
-const Navitems = () => {
-    const pathname = usePathname()
-    const isActive = (path: string) => {
-      if (!pathname) return false;
-      return pathname === path || pathname.startsWith(path + '/');
-    }
+const NavItems = ({
+  initialStocks,
+}: {
+  initialStocks: StockWithWatchlistStatus[];
+}) => {
+  const pathname = usePathname();
+
+  const isActive = (path: string) => {
+    if (path === "/") return pathname === "/";
+
+    return pathname.startsWith(path);
+  };
+
   return (
     <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
-      {NAV_ITEMS.map(({ href, label }) => (
-        <li key={href} className="inline-block mr-6">
-          <Link
-            href={href}
-            className={`hover:text-yellow-500 transition-colors ${
-              isActive(href) ? "text-gray-100" : ""
-            }`}
-          >
-            {label}
-          </Link>
-        </li>
-      ))}
+      {NAV_ITEMS.map(({ href, label }) => {
+        if (href === "/search")
+          return (
+            <li key="search-trigger">
+              <SearchCommand
+                renderAs="text"
+                label="Search"
+                initialStocks={initialStocks}
+              />
+            </li>
+          );
+
+        return (
+          <li key={href}>
+            <Link
+              href={href}
+              className={`hover:text-yellow-500 transition-colors ${
+                isActive(href) ? "text-gray-100" : ""
+              }`}
+            >
+              {label}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 };
-
-export default Navitems;
+export default NavItems;
